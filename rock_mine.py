826d0c28e0b10f3c401512da4b896f8006d08d9a
kesavamahitha <141658357+kesavamahitha@users.noreply.github.com>
2024-01-02 19:05:32 +0530

    Update rock_mine.py

--- rock_mine.py
+++ rock_mine.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-filepath = r"C:\Users\Mahitha\Desktop\sonar_data.csv"
+filepath = r"sonar data.csv"
 data = pd.read_csv(filepath, header = None)
 X = data.drop(columns = 60, axis = 1)
 Y = data[60]
